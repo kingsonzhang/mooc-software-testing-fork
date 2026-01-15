7d1272f598f5d9ab8d4ecb55df094bc3c10b15f3
Kingson Zhang <kingson.x.zhang@gmail.com>
2026-01-14 18:05:46 -0800

    GettingStarted JUnit test case complete

--- src/test/java/tudelft/gettingstarted/GettingStartedTest.java
+++ src/test/java/tudelft/gettingstarted/GettingStartedTest.java
@@ -1,27 +1,32 @@
 package tudelft.gettingstarted;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GettingStartedTest {
 
+    private GettingStarted tester;
+    @BeforeEach
+    public void setUp(){
+        tester = new GettingStarted();
+    }
+
     @Test
     public void addFiveTo20() {
-        int result = new GettingStarted().addFive(20);
+        int result = tester.addFive(20);
         Assertions.assertEquals(25,result);
     }
 
-//    UNCOMMENT THE CODE BELOW, AND FILL THE GAPS!
+    @Test
+    public void addFiveToZero() {
+        int result = tester.addFive(0);
+        Assertions.assertEquals(5, result);
+    }
 
-//    @Test
-//    public void addFiveToZero() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(???, result);
-//    }
-//
-//    @Test
-//    public void addFiveToMinus20() {
-//        int result = new GettingStarted().addFive(???);
-//        Assertions.assertEquals(????,result);
-//    }
+    @Test
+    public void addFiveToMinus20() {
+        int result = tester.addFive(-20);
+        Assertions.assertEquals(-15,result);
+    }
 }
